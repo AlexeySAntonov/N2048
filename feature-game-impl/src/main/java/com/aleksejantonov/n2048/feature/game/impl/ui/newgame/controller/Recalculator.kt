56ebdec85f8aeaaa515d0076101bd24b2045036a
Alexey Antonov <alex.antonov150@gmail.com>
2019-06-11 02:32:07 +0300

    2048: Fix diff indices.

--- feature-game-impl/src/main/java/com/aleksejantonov/n2048/feature/game/impl/ui/newgame/controller/Recalculator.kt
+++ feature-game-impl/src/main/java/com/aleksejantonov/n2048/feature/game/impl/ui/newgame/controller/Recalculator.kt
@@ -11,112 +11,116 @@ class Recalculator {
         when (direction) {
             ItemTouchHelper.UP    -> {
                 for (i in 0 until size) {
-                    val cells = arrayOf(resultList[i], resultList[i + size], resultList[i + size * 2], resultList[i + size * 3])
-                    shiftNullsIfPossible(cells = *cells, initialList = resultList, direction = direction)
+                    val currentIndices = listOf(i, i + size, i + size * 2, i + size * 3)
+                    shiftNullsIfPossible(indices = currentIndices, initialList = resultList, direction = direction)
+                    val cells = listOf(resultList[i], resultList[i + size], resultList[i + size * 2], resultList[i + size * 3])
                     when {
-                        allNullsEquality(*cells)     -> {
+                        allNullsEquality(cells)     -> {
                             // noop
                         }
-                        doublePairEquality(*cells)   -> {
+                        doublePairEquality(cells)   -> {
                             resultList[i + size * 3] = list[i + size * 3].copy(value = null)
                             resultList[i + size * 2] = list[i + size * 2].copy(value = null)
                             resultList[i + size] = list[i + size].copy(value = list[i + size * 3].value!! * 2)
                             resultList[i] = list[i].copy(value = list[i].value!! * 2)
                         }
-                        pairEquality(*cells) != null -> {
-                            val indices = pairEquality(*cells)
+                        pairEquality(cells) != null -> {
+                            val indices = pairEquality(cells)
                             resultList[indices!!.first] = resultList[indices.first].copy(value = resultList[indices.first].value!! * 2)
                             resultList[indices.second] = resultList[indices.second].copy(value = null)
                         }
                     }
-                    shiftNullsIfPossible(cells = *cells, initialList = resultList, direction = direction)
+                    shiftNullsIfPossible(indices = currentIndices, initialList = resultList, direction = direction)
                 }
             }
             ItemTouchHelper.DOWN  -> {
                 for (i in 0 until size) {
-                    val cells = arrayOf(resultList[i], resultList[i + size], resultList[i + size * 2], resultList[i + size * 3])
-                    shiftNullsIfPossible(cells = *cells, initialList = resultList, direction = direction)
+                    val currentIndices = listOf(i, i + size, i + size * 2, i + size * 3)
+                    shiftNullsIfPossible(indices = currentIndices, initialList = resultList, direction = direction)
+                    val cells = listOf(resultList[i], resultList[i + size], resultList[i + size * 2], resultList[i + size * 3])
                     when {
-                        allNullsEquality(*cells)     -> {
+                        allNullsEquality(cells)     -> {
                             // noop
                         }
-                        doublePairEquality(*cells)   -> {
+                        doublePairEquality(cells)   -> {
                             resultList[i + size * 3] = list[i + size * 3].copy(value = list[i + size * 3].value!! * 2)
                             resultList[i + size * 2] = list[i + size * 2].copy(value = list[i].value!! * 2)
                             resultList[i + size] = list[i + size].copy(value = null)
                             resultList[i] = list[i].copy(value = null)
                         }
-                        pairEquality(*cells) != null -> {
-                            val indices = pairEquality(*cells)
+                        pairEquality(cells) != null -> {
+                            val indices = pairEquality(cells)
                             resultList[indices!!.second] = resultList[indices.second].copy(value = resultList[indices.second].value!! * 2)
                             resultList[indices.first] = resultList[indices.first].copy(value = null)
                         }
                     }
-                    shiftNullsIfPossible(cells = *cells, initialList = resultList, direction = direction)
+                    shiftNullsIfPossible(indices = currentIndices, initialList = resultList, direction = direction)
                 }
             }
             ItemTouchHelper.LEFT  -> {
                 for (i in 0 until size) {
-                    val cells = arrayOf(resultList[i * size], resultList[i * size + 1], resultList[i * size + 2], resultList[i * size + 3])
-                    shiftNullsIfPossible(cells = *cells, initialList = resultList, direction = direction)
+                    val currentIndices = listOf(i * size, i * size + 1, i * size + 2, i * size + 3)
+                    shiftNullsIfPossible(indices = currentIndices, initialList = resultList, direction = direction)
+                    val cells = listOf(resultList[i * size], resultList[i * size + 1], resultList[i * size + 2], resultList[i * size + 3])
                     when {
-                        allNullsEquality(*cells)     -> {
+                        allNullsEquality(cells)     -> {
                             // noop
                         }
-                        doublePairEquality(*cells)   -> {
+                        doublePairEquality(cells)   -> {
                             resultList[i * size + 3] = list[i * size + 3].copy(value = null)
                             resultList[i * size + 2] = list[i * size + 2].copy(value = null)
                             resultList[i * size + 1] = list[i * size + 1].copy(value = list[i * size + 3].value!! * 2)
                             resultList[i * size] = list[i * size].copy(value = list[i * size].value!! * 2)
                         }
-                        pairEquality(*cells) != null -> {
-                            val indices = pairEquality(*cells)
+                        pairEquality(cells) != null -> {
+                            val indices = pairEquality(cells)
                             resultList[indices!!.first] = resultList[indices.first].copy(value = resultList[indices.first].value!! * 2)
                             resultList[indices.second] = resultList[indices.second].copy(value = null)
                         }
                     }
-                    shiftNullsIfPossible(cells = *cells, initialList = resultList, direction = direction)
+                    shiftNullsIfPossible(indices = currentIndices, initialList = resultList, direction = direction)
                 }
             }
             ItemTouchHelper.RIGHT -> {
                 for (i in 0 until size) {
-                    val cells = arrayOf(resultList[i * size], resultList[i * size + 1], resultList[i * size + 2], resultList[i * size + 3])
-                    shiftNullsIfPossible(cells = *cells, initialList = resultList, direction = direction)
+                    val currentIndices = listOf(i * size, i * size + 1, i * size + 2, i * size + 3)
+                    shiftNullsIfPossible(indices = currentIndices, initialList = resultList, direction = direction)
+                    val cells = listOf(resultList[i * size], resultList[i * size + 1], resultList[i * size + 2], resultList[i * size + 3])
                     when {
-                        allNullsEquality(*cells)     -> {
+                        allNullsEquality(cells)     -> {
                             // noop
                         }
-                        doublePairEquality(*cells)   -> {
+                        doublePairEquality(cells)   -> {
                             resultList[i * size + 3] = list[i * size + 3].copy(value = list[i * size + 3].value!! * 2)
                             resultList[i * size + 2] = list[i * size + 2].copy(value = list[i * size].value!! * 2)
                             resultList[i * size + 1] = list[i * size + 1].copy(value = null)
                             resultList[i * size] = list[i * size].copy(value = null)
                         }
-                        pairEquality(*cells) != null -> {
-                            val indices = pairEquality(*cells)
+                        pairEquality(cells) != null -> {
+                            val indices = pairEquality(cells)
                             resultList[indices!!.second] = resultList[indices.second].copy(value = resultList[indices.second].value!! * 2)
                             resultList[indices.first] = resultList[indices.first].copy(value = null)
                         }
                     }
-                    shiftNullsIfPossible(cells = *cells, initialList = resultList, direction = direction)
+                    shiftNullsIfPossible(indices = currentIndices, initialList = resultList, direction = direction)
                 }
             }
         }
         return resultList
     }
 
-    private fun allNullsEquality(vararg cells: Cell): Boolean {
+    private fun allNullsEquality(cells: List<Cell>): Boolean {
         var allNulls = true
         for (cell in cells) if (cell.value != null) allNulls = false
         return allNulls
     }
 
-    private fun doublePairEquality(vararg cells: Cell): Boolean {
+    private fun doublePairEquality(cells: List<Cell>): Boolean {
         return cells[0].value != null && cells[0].value == cells[1].value
                 && cells[2].value != null && cells[2].value == cells[3].value
     }
 
-    private fun pairEquality(vararg cells: Cell): Pair<Int, Int>? {
+    private fun pairEquality(cells: List<Cell>): Pair<Int, Int>? {
         // Neighbors
         if (cells[0].value != null && cells[0].value == cells[1].value) return Pair(cells[0].id, cells[1].id)
         if (cells[1].value != null && cells[1].value == cells[2].value) return Pair(cells[1].id, cells[2].id)
@@ -134,15 +138,15 @@ class Recalculator {
         return null
     }
 
-    private fun shiftNullsIfPossible(vararg cells: Cell, initialList: MutableList<Cell>, direction: Int) {
+    private fun shiftNullsIfPossible(indices: List<Int>, initialList: MutableList<Cell>, direction: Int) {
         when (direction) {
             ItemTouchHelper.UP    -> {
-                for (i in 0 until cells.size - 1) {
-                    if (initialList[cells[i].id].value == null) {
-                        for (j in i + 1 until cells.size) {
-                            if (cells[j].value != null) {
-                                initialList[cells[i].id] = initialList[cells[i].id].copy(value = initialList[cells[j].id].value)
-                                initialList[cells[j].id] = initialList[cells[j].id].copy(value = null)
+                for (i in 0 until indices.size - 1) {
+                    if (initialList[indices[i]].value == null) {
+                        for (j in i + 1 until indices.size) {
+                            if (initialList[indices[j]].value != null) {
+                                initialList[indices[i]] = initialList[indices[i]].copy(value = initialList[indices[j]].value)
+                                initialList[indices[j]] = initialList[indices[j]].copy(value = null)
                                 break
                             }
                         }
@@ -150,12 +154,12 @@ class Recalculator {
                 }
             }
             ItemTouchHelper.DOWN  -> {
-                for (i in cells.size - 1 downTo 1) {
-                    if (initialList[cells[i].id].value == null) {
+                for (i in indices.size - 1 downTo 1) {
+                    if (initialList[indices[i]].value == null) {
                         for (j in i - 1 downTo 0) {
-                            if (cells[j].value != null) {
-                                initialList[cells[i].id] = initialList[cells[i].id].copy(value = initialList[cells[j].id].value)
-                                initialList[cells[j].id] = initialList[cells[j].id].copy(value = null)
+                            if (initialList[indices[j]].value != null) {
+                                initialList[indices[i]] = initialList[indices[i]].copy(value = initialList[indices[j]].value)
+                                initialList[indices[j]] = initialList[indices[j]].copy(value = null)
                                 break
                             }
                         }
@@ -163,12 +167,12 @@ class Recalculator {
                 }
             }
             ItemTouchHelper.LEFT  -> {
-                for (i in 0 until cells.size - 1) {
-                    if (initialList[cells[i].id].value == null) {
-                        for (j in i + 1 until cells.size) {
-                            if (cells[j].value != null) {
-                                initialList[cells[i].id] = initialList[cells[i].id].copy(value = initialList[cells[j].id].value)
-                                initialList[cells[j].id] = initialList[cells[j].id].copy(value = null)
+                for (i in 0 until indices.size - 1) {
+                    if (initialList[indices[i]].value == null) {
+                        for (j in i + 1 until indices.size) {
+                            if (initialList[indices[j]].value != null) {
+                                initialList[indices[i]] = initialList[indices[i]].copy(value = initialList[indices[j]].value)
+                                initialList[indices[j]] = initialList[indices[j]].copy(value = null)
                                 break
                             }
                         }
@@ -176,12 +180,12 @@ class Recalculator {
                 }
             }
             ItemTouchHelper.RIGHT -> {
-                for (i in cells.size - 1 downTo 1) {
-                    if (initialList[cells[i].id].value == null) {
+                for (i in indices.size - 1 downTo 1) {
+                    if (initialList[indices[i]].value == null) {
                         for (j in i - 1 downTo 0) {
-                            if (cells[j].value != null) {
-                                initialList[cells[i].id] = initialList[cells[i].id].copy(value = initialList[cells[j].id].value)
-                                initialList[cells[j].id] = initialList[cells[j].id].copy(value = null)
+                            if (initialList[indices[j]].value != null) {
+                                initialList[indices[i]] = initialList[indices[i]].copy(value = initialList[indices[j]].value)
+                                initialList[indices[j]] = initialList[indices[j]].copy(value = null)
                                 break
                             }
                         }
